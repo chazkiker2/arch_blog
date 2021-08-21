5d34570c1bf92e60b48bc71380de62a548411508
chaz-kiker <chaz-kiker@lambdastudents.com>
2021-08-21 13:30:57 -0500

    init commit

---
+++ arch_blog/wsgi.py
@@ -0,0 +1,24 @@
+"""
+WSGI config for arch_blog project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+from dotenv import load_dotenv
+
+from django.core.wsgi import get_wsgi_application
+
+project_folder = os.path.expanduser('~/arch_blog')
+
+print(os.path)
+print(project_folder)
+
+load_dotenv(os.path.join(project_folder, '.env'))
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'arch_blog.settings')
+
+application = get_wsgi_application()
